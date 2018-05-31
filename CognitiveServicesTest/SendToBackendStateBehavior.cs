495f5a9b9b89783bed88f008d4d83149dd043de1
Matteo Finistauri <matteo.finistauri@avanade.com>
2018-05-31 19:45:38 +0200

    Code refactoring.

--- CognitiveServicesTest/SendToBackendStateBehavior.cs
+++ CognitiveServicesTest/SendToBackendStateBehavior.cs
@@ -4,13 +4,25 @@ using System.Collections.Generic;
 
 namespace CognitiveServices
 {
+    /// <summary>
+    ///
+    /// </summary>
+    /// <seealso cref="CognitiveServices.LanguageUnderstanding.BaseStateBehavior" />
     public class SendToBackendStateBehavior : BaseStateBehavior
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SendToBackendStateBehavior"/> class.
+        /// </summary>
+        /// <param name="state"></param>
+        /// <param name="context">The context.</param>
         public SendToBackendStateBehavior(FlowState state, Dictionary<string, object> context)
             : base(state, context)
         {
         }
 
+        /// <summary>
+        /// Executes the behavior.
+        /// </summary>
         public override void ExecuteBehavior()
         {
             Console.WriteLine("Sent to the backend.");
